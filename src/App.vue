db3f5aefae1c2f3bb606077891e886725c21e1e8
Anthony Fu <anthonyfu117@hotmail.com>
2020-11-06 14:58:16 +0800

    refactor: change the order of script and template

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,3 @@
-<!-- the new ref sugar RFC, try it now! -->
-
-<template>
-  <button @click="inc">{{ count }} * 2 = {{ doubled }}</button>
-</template>
-
 <script setup>
 import { computed } from 'vue'
 
@@ -11,4 +5,11 @@ ref: count = 0
 ref: doubled = computed(() => count * 2)
 
 const inc = () => count++
+const dec = () => count -= 1
 </script>
+
+<template>
+  <div>{{ count }} * 2 = {{ doubled }}</div>
+  <button @click="dec">Dec</button>
+  <button @click="inc">Inc</button>
+</template>
